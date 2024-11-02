5ed69c97c3efc65689e44b2bda5511940868041c
FloreDenis2001 <floredenis907@yahoo.com>
2024-11-02 20:14:53 +0200

    update

---
+++ src/main/java/com/example/tecnoscloud/TecnosCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.tecnoscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnosCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnosCloudApplication.class, args);
+	}
+
+}
